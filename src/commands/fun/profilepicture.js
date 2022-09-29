b0c7a30349d6b51b37cc3e5168585b4efddc21d0
MagnoBE <blank@mail.ext>
2022-09-29 19:00:14 +0200

    formatting

--- src/commands/fun/profilepicture.js
+++ src/commands/fun/profilepicture.js
@@ -5,11 +5,9 @@ module.exports = {
     description: 'Show profile picture of user in big size',
     usage: "[User]",
     run: async ({ client, message, args }) => {
-
-
         const { resolveMember } = require('../../functions/parameters');
         let targetMember = await resolveMember(message, args[0], true)
 
-        message.channel.send(targetMember.user.displayAvatarURL({ size: 256, dynamic: true}))
+        message.channel.send(targetMember.user.displayAvatarURL({ size: 256, dynamic: true }))
     }
 }
